15d8af1d0969d7e8a8db9ac9ab866f55459a909f
akramiga <akramlubega73@gmail.com>
2024-12-11 15:17:33 +0300

    basic operations commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+multiply = number1 * number2
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtract}")
+print(f"Multiplication of {number1} and {number2} is {multiply}")
