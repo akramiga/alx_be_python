50fd3a850c91c4aad9b311156b59cf0217ebeb71
akramiga <akramlubega73@gmail.com>
2025-01-11 17:14:12 +0300

    added import module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,4 +1,4 @@
-import datetime 
+from datetime import datetime 
 def display_current_datetime():
     current_date = datetime.datetime.now()
     current_date_str= current_date.strftime("%Y-%m-%d %H:%M:%S")
