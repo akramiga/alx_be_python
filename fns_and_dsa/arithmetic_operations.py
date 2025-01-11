cb44b0b168b648ab72c6d42546ed4500ffba55ac
akramiga <akramlubega73@gmail.com>
2025-01-11 13:08:56 +0300

    correction on line 8

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation == "divide" and num2 or num1 == 0:
+    elif operation == "divide" and num1 or num2 == 0:
         print("Cannot divide by zero!!!!")
     else:
         return num1 / num2    
