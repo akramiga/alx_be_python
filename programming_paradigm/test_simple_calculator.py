aa37348849d3980976e417ee156b9d105cd82aca
akramiga <akramlubega73@gmail.com>
2025-01-19 17:47:14 +0300

    new calc file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,18 +3,23 @@ from simple_calculator import SimpleCalculator
 
 #this is our tester
 class TestOperator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = SimpleCalculator()
+
     def test_add(self):
-        self.assertEqual(SimpleCalculator.add(2,2), 4)
+        self.assertEqual(self.calculator.add(2,2), 4)
 
     def test_subtract(self):
-        self.assertEqual(SimpleCalculator.subtract(2,2), 0)
+        self.assertEqual(self.calculator.subtract(2,2), 0)
 
     def test_multiply(self):
-        self.assertEqual(SimpleCalculator.multiply(2,4), 8)
+        self.assertEqual(self.calculator.multiply(2,4), 8)
 
     def test_division(self):
-        self.assertRaises(SimpleCalculator.divide(10, 5), 2)
+        self.assertEqual(self.calculator.divide(10, 5), 2)
 
     def test_division_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            _ = SimpleCalculator.divide(1 , 0)      
+            _ = self.calculator.divide(1 , 0)      
+if __name__ == "__main__":
+    unittest.main()
