1062f30aab62ae5735c00680213920f2c8acaac2
akramiga <akramlubega73@gmail.com>
2025-01-19 17:54:11 +0300

    new commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,22 +4,22 @@ from simple_calculator import SimpleCalculator
 #this is our tester
 class TestOperator(unittest.TestCase):
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(2,2), 4)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2,2), 4)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(2,2), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2,2), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(2,4), 8)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2,4), 8)
 
-    def test_division(self):
-        self.assertEqual(self.calculator.divide(10, 5), 2)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
     def test_division_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            _ = self.calculator.divide(1 , 0)      
+            _ = self.calc.divide(1 , 0)      
 if __name__ == "__main__":
     unittest.main()
